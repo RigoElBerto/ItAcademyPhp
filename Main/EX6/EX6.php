a374bdded1bad138f1225ce8f205fa3a54b0bb80
RigoElBerto <93231967+RigoElBerto@users.noreply.github.com>
2024-07-01 11:35:22 +0200

    EX6 (POO2)
    Segona part de la poo(r)

---
+++ Main/EX6/EX6.php
@@ -0,0 +1,37 @@
+<?php
+
+
+class Animal{
+private $nom;
+
+public function __construct($nom){
+    $this->nom = $nom;
+}
+
+public function getNom(){
+    return $this->nom;
+}
+public function MakeSound(){
+
+}
+}
+
+class Dog extends Animal{
+
+    public function MakeSound(){
+        return "Bup Bup";
+    }
+}
+
+class Cat extends Animal{
+
+    public function MakeSound(){
+        return "Marrameu Torracastanyes";
+    }
+}
+
+$dogo = new Dog("Snoopy");
+$car = new Cat("HUH");
+
+echo "Em dic " . $dogo->getNom() ." i faig " . $dogo->MakeSound() . "<br>";
+echo "Em dic " . $car->getNom() ." i faig " . $car->MakeSound();
